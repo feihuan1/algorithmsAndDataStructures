b89071f96de30b0c171b8f676c820a8f96e8323e
eric <you@example.comericrealtor1997@gmail.com>
2024-02-13 02:59:34 -0800

    heaps algorithm

--- 0Writeup.js
+++ 0Writeup.js
@@ -1,15 +1,4 @@
-var firstMissingPositive = function(nums) {
-    const range = nums.length + 1; 
-    const set = new Set(nums); 
-    console.log(set)
-    for( let i = 1; i < range + 1; i ++) { 
-        console.log(i)
-        console.log(set.has(i))
-        if(!set.has(i)) {
-            return i
-        }
-        
-    }
-};
+let a = 1
+console.log(a)
 
-console.log(firstMissingPositive([1]))
+a = 2
--- 46Permutations.js
+++ 46Permutations.js
@@ -2,13 +2,49 @@
 
 // Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 
-var permute = function(nums) {
-    if(nums.length === 1) return [nums]; 
-    if(nums.length === 2) return [[nums[0],nums[1]],[nums[1], nums[0]]] 
+// heap's algorithm time O(n!) "order of factorial of n (n x n-1 x ... x 1)", space O(n)
 
-    return nums.map((n) => {
-        const rest = nums.filter(i => i !== n); 
-        const permuteRest = permute(rest); 
-        return permuteRest.map(i => [n, ...i])
-    }).flat(1)
-};
+// Function to generate all permutations of an array
+function permute(nums) {
+    // Function to swap two elements in an array
+    function swap(array, i, j) {
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    // Initialize an array to store permutations
+    const result = [];
+
+    // Function to generate permutations recursively
+    function generatePermutations(array, n) {
+        // Base case: if there's only one element left, we've found a permutation
+        if (n === 1) {
+            // Push a copy of the current permutation to the result array
+            result.push(array.slice());
+            return;
+        }
+            // Generate permutations for the remaining elements
+            generatePermutations(array, n - 1);
+
+        // Recursive case: generate permutations by swapping elements
+        for (let i = 0; i < n - 1; i++) {
+
+            // Swap the i-th element with the last element (n - 1)
+            if (n % 2 === 0) {
+                // If n is even, swap the i-th element with the last element
+                swap(array, i, n - 1);
+            } else {
+                // If n is odd, swap the first element with the last element
+                swap(array, 0, n - 1);
+            }
+            generatePermutations(array, n - 1);
+        }
+    }
+
+    // Generate permutations recursively
+    generatePermutations(nums, nums.length);
+
+    // Return the array of permutations
+    return result;
+}
